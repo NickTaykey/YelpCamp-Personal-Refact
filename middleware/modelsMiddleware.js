176f7ce60ea2dcdf50f3a28f090c03278f2f7271
Nick Tk <nico.toccane@gmail.com>
2019-11-06 21:45:23 +0100

    add error handling to edit and update

--- middleware/modelsMiddleware.js
+++ middleware/modelsMiddleware.js
@@ -29,15 +29,24 @@ let middlewareOBJ = {
     }
   },
   validateImgs(req, res, next) {
+    const context = req.method === "PUT" ? true : false;
     if (req.files.length) {
       // iteriamo su req.files
       for (const file of req.files) {
         // se il file non è valido facciamo redirect con flash msg
         if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+          const campgroud = context ? req.body.campground : req.body;
           // settaggio dei cookie
-          res.cookie("name", req.body.name);
-          res.cookie("description", req.body.description);
-          res.cookie("price", req.body.price);
+          res.cookie("name", campgroud.name);
+          res.cookie("description", campgroud.description);
+          res.cookie("price", campgroud.price);
+          if (context) {
+            let i = 0;
+            for (const delImg of req.body.deleteImages) {
+              res.cookie(`deleteImg${i}`, delImg);
+              i++;
+            }
+          }
           // redirect con flash message
           req.flash(
             "error",
@@ -48,7 +57,7 @@ let middlewareOBJ = {
       }
       // se tutti i file sono validi invochiamo next
       next();
-    } else if (req.method !== "PUT") {
+    } else if (!context) {
       // settaggio dei cookie
       res.cookie("name", req.body.name);
       res.cookie("description", req.body.description);
@@ -61,6 +70,15 @@ let middlewareOBJ = {
     res.clearCookie("name");
     res.clearCookie("description");
     res.clearCookie("price");
+    let delImgs = [];
+    if (req.path.split("/")[2] === "edit")
+      for (let i = 0; i < 4; i++) {
+        if (req.cookies[`deleteImg${i}`]) {
+          res.clearCookie(`deleteImg${i}`);
+          delImgs.push(req.cookies[`deleteImg${i}`]);
+        }
+      }
+    res.locals.delImgs = delImgs;
     res.locals.name = req.cookies.name;
     res.locals.description = req.cookies.description;
     res.locals.price = req.cookies.price;
