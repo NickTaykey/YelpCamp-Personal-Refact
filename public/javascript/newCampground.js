240b702c4fa4c3857d41dacbb44edff293b3636f
Nick Tk <nico.toccane@gmail.com>
2019-11-09 17:47:32 +0100

    add image number validation

---
+++ public/javascript/newCampground.js
@@ -0,0 +1,8 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  let images = document.querySelector("#images");
+  if (images.files.length > 4) {
+    event.preventDefault();
+    alert("You can upload at the most 4 images!");
+  }
+});
