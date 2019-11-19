78d99ba5121fc5b678c2b00d67f2b81180a7c097
Nick Tk <nico.toccane@gmail.com>
2019-11-19 21:52:02 +0100

    complete UI features bar feature

--- public/javascripts/togleOption.js
+++ public/javascripts/togleOption.js
@@ -1,5 +1,25 @@
+/* 
+  ATTENZIONE!
+
+  OGNI VOLTA CHE NASCONDIAMO UNA MAPPA (il div con id map) E LA FACCIAMO RIAPPARIRE DOBBIAMO RICHIAMARE IL 
+  METODO resize() SUL OGGETTO map CHE LA RAPPRESENTA, ALTRIMENTI LA MAPPA NON VIENE RENDERIZZATA CON LE 
+  DIMENSIONI CORRETTE CHE ABBIAMO SPECIFICATO DAL CSS
+*/
+
 // selezioniamo tutte le opzioni
-let options = document.querySelectorAll(".list-group-item");
+let options = document.querySelectorAll(".list-group-item"),
+  // classi di tutti gli elementi che POSSIAMO nascondere
+  elems = [".geo", ".reviews", ".info", ".images"],
+  // ELEMENTO DA MOSTRARE
+  elemToShow,
+  // RIMUOVE DAL ARRAY CON GLI ELEMENTI CHE POSSIAMO NASCONDERE
+  // QUELLO CHE NON VOGLIAMO NASCONDERE E RITORNA L'ARRAY MODIFICATO
+  getElementsToHide = el => {
+    let arr = [...elems];
+    arr.splice(elems.indexOf(el), 1);
+    return arr;
+  };
+
 for (const opt of options) {
   // applichiamo un click su tutte le opzioni
   opt.addEventListener("click", function() {
@@ -11,37 +31,38 @@ for (const opt of options) {
         .classList.remove("active");
       // gliela aggiungiamo
       this.classList.add("active");
-      let clsToDisplay, clsToHide;
-      // per ogni classe da mostrare trova il suo nome ed il nome delle classi degli elementi da nascondere
-      if (this.textContent === "General Info") {
-        clsToDisplay = ".info";
-        clsToHide = ".images, .geo, .reviews";
-      } else if (this.textContent === "Photos") {
-        clsToDisplay = ".images";
-        clsToHide = ".info, .geo, .reviews";
-      } else if (this.textContent === "Location") {
-        clsToDisplay = ".geo";
-        clsToHide = ".images, .info, .reviews";
-      } else if (this.textContent === "Reviews") {
-        clsToDisplay = ".reviews";
-        clsToHide = ".images, .geo, .info";
+      // recuperiamo il testo del elemento
+      let text = this.textContent;
+      // constrolliamo il testo IN BASE AL NOME DEL OPZIONE SPECIFICHIAMO L'ELEMENTO DA MOSTRARE
+      if (text === "General Info") {
+        elemToShow = ".info";
+      } else if (text === "Location") {
+        elemToShow = ".geo";
+      } else if (text === "Photos") {
+        elemToShow = ".images";
+      } else if (text === "Reviews") {
+        elemToShow = ".reviews";
       }
-      // mostra tutti gli elementi con la classe da mostrare
-      let els = document.querySelectorAll(clsToDisplay);
-      for (const el of els) {
-        // quando cambiamo il valore di display della mappa x mostrarla chiamiamo sempre il metodo resize per
-        // ridimensionarla e farla apparire della grandezza giusta
+      // MOSTRIAMO TUTTI GLI ELEMENTI DA MOSTRARE
+      for (const el of document.querySelectorAll(elemToShow)) {
         el.style.display = "block";
+        // SELEZIONIAMO IL THUMBNAIL
         let thumbnail = document.querySelector(".thumbnail");
-        if (clsToDisplay === ".geo") map.resize();
-        else if (clsToDisplay === ".reviews") thumbnail.style.display = "none";
-        else thumbnail.style.display = "block";
+        // SE L'ELEMENTO DA MOSTRARE E' .reviews NASCONDIAMO IL THUMBNAIL
+        if (elemToShow === ".reviews") thumbnail.style.display = "none";
+        else {
+          // ALTIRMENTI MOSTRIAMO IL THUMBNAIL
+          thumbnail.style.display = "block";
+          // SE L'ELEMENTO DA MOSTRARE E' LA MAPPA ALLORA LA RIDIMENSIONIAMO
+          // (altimenti nn apparirebbe con le dimensioni corrette)
+          if (elemToShow === ".geo") map.resize();
+        }
       }
-      // nasconde tutti gli elementi con la classe da nascondere
-      els = document.querySelectorAll(clsToHide);
-      for (const el of els) {
+      // selezioniamo tutti gli elementi da nascondere
+      elemsTohide = getElementsToHide(elemToShow);
+      // li nascondiamo
+      for (const el of document.querySelectorAll(elemsTohide))
         el.style.display = "none";
-      }
     }
   });
 }
