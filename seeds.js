0ce88b8f4158b8085e580d418c124b3df7ae0956
Nick Tk <nico.toccane@gmail.com>
2020-01-02 20:14:12 +0100

    complete last feature

--- seeds.js
+++ seeds.js
@@ -1,17 +1,34 @@
-const Campground = require("./models/campground");
 const faker = require("faker");
+const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+const User = require("./models/user");
 
 module.exports = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 40; i++) {
-    await Campground.create({
+  await Comment.deleteMany({});
+  // user to associate with any campground or comment
+  let { _id, username } = await User.findById("5e0a5dbf787e9706c984c95f");
+  let i = 0;
+  while (i < 40) {
+    let campgound = await Campground.create({
       name: faker.lorem.words(5),
-      price: Math.random() * 101,
-      description: faker.lorem.paragraphs(4),
+      price: Math.floor(Math.random() * 101),
+      description: faker.lorem.words(20),
       location: "US",
       placeName: faker.random.words(2),
-      author: "5dec03ff333d0209b0009a5d"
+      author: { id: _id, username }
     });
+    for (let i = 0; i < 20; i++) {
+      let comment = await Comment.create({
+        text: faker.lorem.words(5),
+        rating: Math.floor(Math.random() * 6),
+        author: { id: _id, username }
+      });
+      campgound.comments.push(comment);
+    }
+    await campgound.calcAvgRating();
+    await campgound.save();
+    i++;
   }
   console.log("40 new campgrounds created");
 };
